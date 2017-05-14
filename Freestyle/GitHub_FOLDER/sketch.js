341c6f78299e0a8434fb7488c5f372fdfd575522
Marcus Due <duendue@hotmail.dk>
2017-05-14 17:57:55 +0200

    Add files via upload

---
+++ Freestyle/GitHub_FOLDER/sketch.js
@@ -0,0 +1,91 @@
+//variable for statement image
+var statementImage;
+
+//These variables are for connected to the statements. Each statement get generated from the date.
+var dateCheck;
+var apiDate = 0;
+var crimeDate;
+var humDate;
+
+//Variables for each type of statement
+var SpinnedStatement;
+var RawDataType;
+var MasterUrl
+
+//Preload of the official government legislation image
+function preload() {
+   statementImage = loadImage('assets/background.jpg');
+}
+
+function setup() {
+    createCanvas(1000, 1000);
+
+    background(255);
+
+    //This variable is defined from the date of today.
+    dateCheck = day();
+    
+    //This specifies how often there will be gathered a new API and when to take a picture of the raw data.
+    setInterval(apiSwitch, 10000);
+    setTimeout(canvasGrab, 30000);
+    
+    fill(80);
+    textSize(22);
+}
+
+var SpinnedTimer = 0;
+
+//This is the master function for the main text segments. It gathers data from the api variables and write statements from them
+function Statement(){
+  if(SpinnedTimer >= 1){
+    push();
+    fill(80);
+    textSize(22);
+    textFont("Orator Std");
+    background(235);
+    image(statementImage, 0, 0);
+    textAlign(LEFT);
+    textStyle(NORMAL);
+    text(GenericStatement, 125, 365, 700, 480);
+    textStyle(BOLD);
+    text(SpinnedStatement, 125, 635, 700, 500);
+    pop();
+  }
+  else
+  {
+    background(235);
+    textStyle(NORMAL);
+    textFont("Rockwell");
+    textAlign(CENTER);
+    text(RawDataType, 125, 500, 700, 350);
+    push();
+    textSize(15);
+    text("Data Source: " + MasterUrl, 125, 650, 700, 200);
+    text(timerWat, 150, 150);
+    pop();
+  }
+  setTimeout(incrementer, 1000*30);
+  
+  //Debug Log Block
+  console.log("-------------------------------------------------------------------------------------");
+  console.log("Dag: " + dateCheck);
+  console.log("Generic: " + GenericStatement);
+  console.log("Spin: " + SpinnedStatement);
+  console.log("RåData: " + RawDataType);
+  console.log("-------------------------------------------------------------------------------------");
+}
+var timerWat = 0;
+
+function draw(){
+  timerWat++
+}
+
+function incrementer(){
+  SpinnedTimer++;
+}
+
+//This functions grabs a picture of the canvas while it only displays the rawdata. Afterwards the canvas gets enlarged to display the whole image.
+function canvasGrab(){
+  saveCanvas('rawData/RawStatement.png', 'png');
+  console.log("Picture has been grabbed!");
+}
